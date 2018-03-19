471445c0ef6ab706ecc8b38515a3ab39f7e37a49
wangjinzhao <wangjinzhao@kingsoft.com>
2018-03-19 22:43:14 +0800

    查找链表中间节点，判断链表是否有环

--- javacore/src/main/java/com/jinzhao/wang/javacore/algorithm/LinkedList.java
+++ javacore/src/main/java/com/jinzhao/wang/javacore/algorithm/LinkedList.java
@@ -35,7 +35,9 @@ public class LinkedList {
         }
     }
 
-
+    /**
+     * 翻转链表
+     */
     public Node reverse(Node head) {
         if (head == null)
             return head;
@@ -54,23 +56,62 @@ public class LinkedList {
         return pre;
     }
 
+    /**
+     * 查找链表中间节点
+     */
+    public Node searchMidNode(Node head) {
+        if (head == null)
+            return null;
+        Node fastNode = head.next;
+        Node slowNode = head.next;
+        while (fastNode.next != null && fastNode.next.next != null) {
+            fastNode = fastNode.next.next;
+            slowNode = slowNode.next;
+
+        }
+        System.out.println(slowNode.data);
+        return slowNode;
+    }
+
+    /**
+     *链表有环
+     */
+    public boolean circularLinkedList(Node node){
+        if (node==null||node.next==null) return false;
+        if (node==node.next) return true;
+        Node slow=node;
+        Node fast=node;
+        while (fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if (slow==fast)
+                return true;
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
-        LinkedList linkedList=new LinkedList();
+        LinkedList linkedList = new LinkedList();
         linkedList.insert(1);
         linkedList.insert(2);
         linkedList.insert(3);
-        linkedList.insert(5);
         linkedList.insert(4);
+        linkedList.insert(5);
+        linkedList.insert(6);
+//        linkedList.insert(7);
+//        linkedList.insert(8);
+//        linkedList.insert(9);
 
-        Node reverseHead=linkedList.reverse(linkedList.head);
-
-        Node current=reverseHead;
-        while (current!=null){
-            System.out.println(current.data);
-            current=current.next;
-
-
-        }
+//        Node reverseHead = linkedList.reverse(linkedList.head);
+//
+//        Node current = reverseHead;
+//        while (current != null) {
+//            System.out.println(current.data);
+//            current = current.next;
+//        }
+ //       linkedList.searchMidNode(linkedList.head);
+        System.out.println(linkedList.circularLinkedList(linkedList.head));
     }
 
 }
